50844344f7e9c154164f80c935ac0c4be5bf0c46
austinschroeder <austinsoundy@gmail.com>
2021-11-20 15:16:37 -0800

    added links component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   // localhost: 3000
@@ -11,6 +12,7 @@ function App() {
 
   return (
     <div>
+      <MainNavigation />
       <Switch>
         {/* Exact keeps from always loading '/' */}
         <Route path="/" exact>
