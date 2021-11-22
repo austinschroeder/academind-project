a6df91962340f715c7f74ee2f8108915dc355553
austinschroeder <austinsoundy@gmail.com>
2021-11-22 12:56:30 -0800

    added entry form

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   // localhost: 3000
   // my-page.com/
 
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
-        {/* Exact keeps from always loading '/' */}
+        {/* 'Exact' keeps from always loading '/' */}
         <Route path="/" exact>
           <AllMeetupsPage />
         </Route>
@@ -27,7 +26,7 @@ function App() {
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
