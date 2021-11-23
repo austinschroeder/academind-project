377ef19b46617b172c1a78ba66143c7487141659
austinschroeder <austinsoundy@gmail.com>
2021-11-23 10:30:57 -0800

    working on 'POST' bug

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,30 @@
+import { useHistory } from 'react-router';
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    // Firebase requires .json
+    fetch(
+      'https://react-meetups-4636f-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
